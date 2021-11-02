8284c9414b302cce3d388040a218c9b294ae722e
Alvaro Barba <abarfer480@g.educaand.es>
2021-11-02 14:17:45 +0100

    Actualizacion ejercicios.

---
+++ Objetos/fechas.js
@@ -0,0 +1,43 @@
+/*
+mayor de dos fechas
+dada una fecha mayor de 18
+diferencia en dias de dos fechas
+*/
+date1 = new Date("Novemver 4, 2021 14:30:56");
+date2 = new Date("Novemver 20, 2021 11:15:06");
+
+let anio1 = date1.getYear();
+let mes1 = date1.getMonth();
+let dia1 = date1.getDate();
+
+let anio2 = date2.getYear();
+let mes2 = date2.getMonth();
+let dia2 = date2.getDate();
+
+console.log(anio)
+console.log(mes)
+console.log(dia)
+console.log(hora)
+
+if(anio1 < anio2){
+
+    comsole.log("La fecha ${date2} es mayor.")
+
+} else {
+    if (anio1 = anio2){
+        
+
+    } else {
+        if (anio1 > anio2){
+
+            comsole.log("La fecha ${date1} es mayor.")
+
+        }
+    }
+} 
+
+cad1 = date1.toDateString();
+cad2 = date2.toDateString();
+
+
+
